cb7e9e97a4e5d7e9d2af8ecce03fdf110bf08c94
sanifali@folio3.com <sanifali@folio3.com>
2021-12-28 16:20:30 +0500

    - initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+resource "google_cloudfunctions_function" "function" {
+  provider = google-beta
+
+  name = var.function_name
+  description = var.function_description
+  runtime = "java11"
+
+  available_memory_mb = var.memory
+  source_archive_bucket = var.bucket_name
+  source_archive_object = var.bucket_object
+  entry_point = var.entry_point
+  trigger_http = true
+  environment_variables = var.env_variables
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  provider = google-beta
+  project = google_cloudfunctions_function.function.project
+  region = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "function_name" {
+  type = string
+}
+variable "function_description" {
+  type = string
+}
+variable "bucket_name" {
+  type = string
+}
+variable "bucket_object" {
+  type = string
+}
+variable "entry_point" {
+  type = string
+}
+variable "memory" {
+  type = string
+}
+variable "env_variables" {
+  default = null
+}
